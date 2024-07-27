931cccd8b9a960f0d998aaaff758dfed32d2fcf1
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-07-27 15:54:15 +0900

    job table 생성

---
+++ src/main/generated/kr/co/fifoBack/entity/user/QJob.java
@@ -0,0 +1,39 @@
+package kr.co.fifoBack.entity.user;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QJob is a Querydsl query type for Job
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QJob extends EntityPathBase<Job> {
+
+    private static final long serialVersionUID = -1177287739L;
+
+    public static final QJob job = new QJob("job");
+
+    public final NumberPath<Integer> jno = createNumber("jno", Integer.class);
+
+    public final StringPath jobname = createString("jobname");
+
+    public QJob(String variable) {
+        super(Job.class, forVariable(variable));
+    }
+
+    public QJob(Path<? extends Job> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QJob(PathMetadata metadata) {
+        super(Job.class, metadata);
+    }
+
+}
+
