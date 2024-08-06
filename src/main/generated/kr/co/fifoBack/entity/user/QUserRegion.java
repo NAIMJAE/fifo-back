8bec93ebd93d9789c257cb7fe58211ba01804540
NAIMJAE <skdlawo5985@gmail.com>
2024-08-06 15:48:04 +0900

    aa

--- src/main/generated/kr/co/fifoBack/entity/user/QUserRegion.java
+++ src/main/generated/kr/co/fifoBack/entity/user/QUserRegion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package kr.co.fifoBack.entity.user;
 
 import static com.querydsl.core.types.PathMetadataFactory.*;
@@ -40,46 +39,3 @@ public class QUserRegion extends EntityPathBase<UserRegion> {
 
 }
 
-=======
-package kr.co.fifoBack.entity.user;
-
-import static com.querydsl.core.types.PathMetadataFactory.*;
-
-import com.querydsl.core.types.dsl.*;
-
-import com.querydsl.core.types.PathMetadata;
-import javax.annotation.processing.Generated;
-import com.querydsl.core.types.Path;
-
-
-/**
- * QUserRegion is a Querydsl query type for UserRegion
- */
-@Generated("com.querydsl.codegen.DefaultEntitySerializer")
-public class QUserRegion extends EntityPathBase<UserRegion> {
-
-    private static final long serialVersionUID = 193640855L;
-
-    public static final QUserRegion userRegion = new QUserRegion("userRegion");
-
-    public final StringPath regionname = createString("regionname");
-
-    public final NumberPath<Integer> urno = createNumber("urno", Integer.class);
-
-    public final NumberPath<Integer> userno = createNumber("userno", Integer.class);
-
-    public QUserRegion(String variable) {
-        super(UserRegion.class, forVariable(variable));
-    }
-
-    public QUserRegion(Path<? extends UserRegion> path) {
-        super(path.getType(), path.getMetadata());
-    }
-
-    public QUserRegion(PathMetadata metadata) {
-        super(UserRegion.class, metadata);
-    }
-
-}
-
->>>>>>> 2cb1ccf63ecd4698d19edcce98ff1e3e62ce8bc0
